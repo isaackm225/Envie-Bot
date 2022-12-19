1db9c553fc9caee9d84cc24ef9c8c120b0760698
Envie <kmorelisaac@gmail.com>
2022-12-19 17:32:07 -0500

    TODO: make better document_initialized wait

--- main.py
+++ main.py
@@ -1,24 +1,73 @@
-from selenium import webdriver
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary #this module makes sure the gecko driver is added to the path before execution
-#binary = FirefoxBinary('C:\Firefox\Firefox.exe')
-#driver = webdriver.Firefox(firefox_binary=binary)
 import csv
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
-
-
+from selenium.webdriver.support.wait import WebDriverWait
+# if Firefox:
 """
-#extracting resident code
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary #this module makes sure the gecko driver is added to the path before execution
+#"""
 
-with open("Delinquency_Report_October_4_2022.csv", "r", newline="") as report:
-    reader = csv.DictReader(report,dialect="excel", delimiter=",", quotechar='"',quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        print(row['Code'])
-"""
-"""
+#if Edge:
+#"""
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+#"""
 
-"""
+#CREDENTIALS AND CONSTANTS
+UNAME = input("Email: ")
+PWORD = input("placeholder_p: ")
 
-driver = webdriver.Firefox()
-driver.get("https://www.yardiaspcn6.com/08930ashcro/pages/menu.aspx?sMenuSet=StudentHousing")
+#Waits
+def document_initialized(driver):
+    """Waiting for the doc to be ready"""
+    return driver.execute_script("return document.readyState")
+
+def login_wait(driver):
+    """Waiting for the bot to be logged in"""
+    
+    collection_box = driver.find_element(By.CLASS_NAME, "ion-inherit-color")
+    box_string = collection_box.text
+    if "CA$0.00" not in box_string:
+        return True
+    return False
+    
+def toggle_menu(driver):
+    pages = driver.find_element(By.ID, "ag-2009-of-page-number")
+    return pages
+
+
+#Common Code
+options = Options()
+options.add_experimental_option("detach", True)
+#if Edge:
+# """
+driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
+#"""
+#if Firefox:
+"""
+binary = FirefoxBinary('C:\Firefox\Firefox.exe')
+driver = webdriver.Firefox(firefox_binary=binary)
+#"""
+driver.get("https://manage.tenantpay.com/login")
+WebDriverWait(driver, timeout=10).until(document_initialized)
+
+## login page
+uname_field = driver.find_element(By.NAME, "ion-input-0")
+uname_field.send_keys(UNAME)
+pword_field = driver.find_element(By.NAME, "ion-input-1")
+pword_field.send_keys(PWORD)
+pword_field.send_keys(Keys.ENTER)
+WebDriverWait(driver, timeout=10).until(login_wait)
+
+## landing page
+toogle_menu = driver.find_element(By.CSS_SELECTOR, "ion-list>ion-menu-toggle:nth-of-type(2)")
+toogle_menu.click()
+a = toogle_menu(driver)
+print(a.text)
+#WebDriverWait(driver, timeout=10).until()
+
+#download_button = driver.find_element(By.CSS_SELECTOR, "ion-button.md.button.button-solid.ion-activatable")
+#print(download_button)
+#download_button.click()
